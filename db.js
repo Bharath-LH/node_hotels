6463597634838f87f4773cfc839f634706dfbdab
Bharath <Bharath0894r@gmail.com>
2024-09-26 19:54:46 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,21 +1,27 @@
 const mongoose=require("mongoose");
+require('dotenv').config();
 //Define yoour MongoDb connection url
-const mongoURL='mongodb://localhost:27017/hotel'//Replace this your database
-
-mongoose.connect(mongoURL,{
-    useNewUrlParser:true,
+//const mongoURL='mongodb://localhost:27017/hotel'
+//const mongoURL=process.env.MONGODB_URL_LOCAL //Replace this your database
+const mongoURL=process.env.MONGODB_URL;
+//const mongoURL='mongodb+srv://helloworld:Qwerty12345@cluster0.ukbgf.mongodb.net/'
+mongoose.connect(mongoURL, {
+     useNewUrlParser:true,
     useUnifiedTopology:true
 
-}) 
+})
+
+  
 const db=mongoose.connection;
 
 db.on('connected',()=>{
     console.log("Connected to MongoDB server");
 });
 
-db.on('error',()=>{
-    console.log("MongoDB connection error",err);
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
 });
+
 
 db.on('disconnected',()=>{
     console.log("Disconnected to MongoDB server");
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const db=require('./db');
+require('dotenv').config();
 app.use(express.json()); 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());//req.body
 
+const PORT=process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('welcome to our hotel')
@@ -40,6 +42,7 @@ const menuItemRoutes=require('./routes/menuItemRoutes');
 app.use('/menu',menuItemRoutes)
 
 
-app.listen(3000,()=>{
+
+app.listen(PORT,()=>{
 console.log("Listening on port 3000")
 })
